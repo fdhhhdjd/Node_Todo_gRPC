517a58219e100d3b27ab6159f670a5cd2cda3fe8
fdhhhdjd <nguyentientai10@gmail.com>
2024-07-31 09:35:00 +0700

    fix: path protos

--- src/protos/index.js
+++ src/protos/index.js
@@ -9,13 +9,13 @@ const {
 } = require("@/configs");
 const LoadFile = require("@/helpers/loadFileHelpers");
 
+//* Load File Protos
 const protoDir = path.join(__dirname, "..", "protos");
-
 const protos = LoadFile.loadProtos(protoDir);
-
 const userProto = protos.user;
 const todoProto = protos.todo;
 
+//* Get service client
 const userClient = new userProto.UserService(
   `localhost:${userPort}`,
   grpc.credentials.createInsecure(),
